232d4198b206ee7152e535e24dd8adbf7090f210
13eta <teslawpt13@gmail.com>
2026-08-23 00:20:20 +0900

    Add Ver2.2 face emotion profiles

---
+++ src/v22-config.js
@@ -0,0 +1,18 @@
+export const EMOTIONS = {
+  neutral:{ko:'기본',head:0,body:0,armL:0,armR:0,energy:.10,eyeL:1,eyeR:1,mouth:.02,smile:.05,browL:0,browR:0},
+  happy:{ko:'행복',head:-2,body:.8,armL:-2.5,armR:2.5,energy:.35,eyeL:.62,eyeR:.62,mouth:.18,smile:.90,browL:.18,browR:.18},
+  excited:{ko:'신남',head:1,body:0,armL:-9,armR:9,energy:1,eyeL:1.08,eyeR:1.08,mouth:.48,smile:.55,browL:.45,browR:.45},
+  teasing:{ko:'장난',head:-6,body:-1.2,armL:-8,armR:1.5,energy:.55,eyeL:1,eyeR:.25,mouth:.08,smile:.72,browL:.30,browR:-.12},
+  pleading:{ko:'울망',head:4,body:.8,armL:3,armR:-3,energy:.12,eyeL:1.08,eyeR:1.08,mouth:.05,smile:-.12,browL:.46,browR:.46},
+  relaxed:{ko:'느긋',head:-3,body:1.2,armL:1,armR:-1,energy:.08,eyeL:.70,eyeR:.70,mouth:.03,smile:.30,browL:-.10,browR:-.10},
+  sick:{ko:'아픔',head:5,body:1.8,armL:1,armR:-1,energy:.03,eyeL:.58,eyeR:.58,mouth:.05,smile:-.42,browL:.26,browR:.26},
+  angry:{ko:'화남',head:-1.5,body:-1.8,armL:3.5,armR:-3.5,energy:.70,eyeL:.72,eyeR:.72,mouth:.05,smile:-.78,browL:-.42,browR:-.42},
+  annoyed:{ko:'삐짐',head:-5,body:1,armL:2,armR:-2,energy:.12,eyeL:.78,eyeR:.68,mouth:.03,smile:-.58,browL:-.18,browR:.10},
+  sad:{ko:'슬픔',head:5,body:1.4,armL:2,armR:-2,energy:.04,eyeL:.90,eyeR:.90,mouth:.04,smile:-.60,browL:.38,browR:.38},
+  surprised:{ko:'놀람',head:0,body:0,armL:-7,armR:7,energy:.82,eyeL:1.13,eyeR:1.13,mouth:.62,smile:0,browL:.62,browR:.62},
+  embarrassed:{ko:'부끄러움',head:3.5,body:.7,armL:2.5,armR:-2.5,energy:.16,eyeL:.78,eyeR:.78,mouth:.07,smile:.42,browL:.22,browR:.22},
+  scared:{ko:'겁남',head:2,body:.9,armL:3.5,armR:-3.5,energy:.65,eyeL:1.10,eyeR:1.10,mouth:.34,smile:-.46,browL:.58,browR:.58},
+  smug:{ko:'의기양양',head:-4,body:-1,armL:1.5,armR:-1.5,energy:.20,eyeL:.70,eyeR:.58,mouth:.03,smile:.68,browL:-.12,browR:.24},
+  confused:{ko:'갸웃',head:6,body:0,armL:-1.5,armR:1.5,energy:.20,eyeL:.96,eyeR:.78,mouth:.04,smile:.08,browL:.42,browR:-.18},
+  love:{ko:'좋아!',head:-2,body:0,armL:2.5,armR:-2.5,energy:.60,eyeL:.60,eyeR:.60,mouth:.20,smile:.95,browL:.22,browR:.22}
+};
